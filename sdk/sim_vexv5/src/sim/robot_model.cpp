0c0530ea75e36939c0352154d44f8912b3d65865
cowsed <richie.sommers@outlook.com>
2023-05-26 22:18:05 -0400

    Reorganized some stuff fir assimp integration

--- sdk/sim_vexv5/src/sim/robot_model.cpp
+++ sdk/sim_vexv5/src/sim/robot_model.cpp
@@ -3,22 +3,6 @@ namespace sim
 {
     namespace construction
     {
-        void drawUI()
-        {
-            ImGui::Begin("Robot Construction");
-            // ImGui::Text("%s", this_robot->robot_name.c_str());
-            // if (ImGui::CollapsingHeader("Structures", ImGuiTreeNodeFlags_DefaultOpen))
-            // {
-            // ImGui::Text("Count: %lu", this_robot->num_structures);
-            // for (auto &s : this_robot->structures)
-            // {
-            // ImGui::Separator();
-            // ImGui::LabelText("name", "%s", s.second.name.c_str());
-            // ImGui::LabelText("id", "%d", s.second.id);
-            // }
-            // }
-            // ImGui::End();
-        }
 
     } // construction
 } // sim
