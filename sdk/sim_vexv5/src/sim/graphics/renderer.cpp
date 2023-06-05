46c2d5efb0f958e6714b7f902e89b4fdba401bf1
cowsed <richie.sommers@outlook.com>
2023-06-05 13:36:17 -0400

    More Motor Design

--- sdk/sim_vexv5/src/sim/graphics/renderer.cpp
+++ sdk/sim_vexv5/src/sim/graphics/renderer.cpp
@@ -29,7 +29,7 @@ namespace sim
         bool main_draw = true;
         bool draw_skybox = true;
 
-        glm::vec3 light_pos = {5,10,5};
+        glm::vec3 light_pos = {5, 10, 5};
 
         RenderTarget field_viewport;
         Camera field_camera(glm::vec3(0, 0, 0.2), 0, 0, field_viewport);
@@ -46,6 +46,7 @@ namespace sim
         sim::physics::phys_id field_id;
 
         construction::Model *bars_shape;
+        construction::Model *motor_shape;
 
         void setup()
         {
@@ -60,28 +61,27 @@ namespace sim
                 t.setOrigin(v);
                 return t;
             };
-            puts("renderer initting\n");
             setup_common();
 
             field_viewport.init(800, 600);
-            std::cout << "field skybox px = " << field_skybox.nx.width << '\n';
             field_skybox.init();
             brain_screen = new brain_screen_shape();
 
-            // MODEL_PATH is defined by the makefile. this guard is only here to make the ide be quiet
-            #ifndef MODEL_PATH
-            #define MODEL_PATH "./"
-            #endif
+// MODEL_PATH is defined by the makefile. this guard is only here to make the ide be quiet
+#ifndef MODEL_PATH
+#define MODEL_PATH "./"
+#endif
 
             auto brain_path = std::string(MODEL_PATH) + std::string("Devices/Brain/V5_Brain.dae");
-            auto nut_path =   std::string(MODEL_PATH) + std::string("Fields/OverUnder/nut.dae");
+            auto nut_path = std::string(MODEL_PATH) + std::string("Fields/OverUnder/nut.dae");
             auto field_path = std::string(MODEL_PATH) + std::string("Fields/OverUnder/field.dae");
-            auto bars_path =  std::string(MODEL_PATH) + std::string("Fields/OverUnder/bars.dae");
+            auto bars_path = std::string(MODEL_PATH) + std::string("Fields/OverUnder/bars.dae");
+            auto motor_path = std::string(MODEL_PATH) + std::string("Devices/Motor/Motor.dae");
 
             brain_shape = new construction::Model(brain_path);
             btTransform brain_transform = btTransFromOrigin(btVectorFromGlm3(glm::vec3(0.1, .3, .4)));
 
-            brain_transform.setRotation(btQuaternion(btVector3{1,0,0}, -.55));
+            brain_transform.setRotation(btQuaternion(btVector3{1, 0, 0}, -.55));
 
             std::unique_ptr<btCollisionShape> brain_collision = brain_shape->make_convex_hull();
             float brain_mass = .11; // kg
@@ -99,8 +99,7 @@ namespace sim
             field_id = physics::add_static_mesh(std::move(field_collision), field_transform, 1.0);
 
             bars_shape = new construction::Model(bars_path);
-            // motor_shape = new construction::Model("Construction/bars.dae");
-
+            motor_shape = new construction::Model(motor_path);
         }
 
         void build_ui()
@@ -131,7 +130,7 @@ namespace sim
 
             glm::mat4 view = field_camera.view_matrix();
             glm::mat4 persp = field_camera.persp_matrix(field_viewport);
-            glm::mat4 ident = {1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1};
+            glm::mat4 ident = {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1};
             if (main_draw)
             {
                 auto brain_trans = physics::get_transform_matrix(brain_id);
@@ -142,9 +141,10 @@ namespace sim
                 nut_shape->render(persp, view, nut_trans, light_pos);
 
                 glm::mat4 field_trans = physics::get_transform_matrix(field_id);
-                field_shape->render(persp, view, field_trans,light_pos);
+                field_shape->render(persp, view, field_trans, light_pos);
 
                 bars_shape->render(persp, view, ident, light_pos);
+                motor_shape->render(persp, view, glm::translate(ident, {0.1, 0.4, 0.3}), light_pos);
             }
             if (phys_debug_draw)
             {
