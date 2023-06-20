f81df4548693325dce64da407a4a602f8a2f7ee3
cowsed <richie.sommers@outlook.com>
2023-06-19 22:47:52 -0400

    fixed warning about ISO c++ not converting string literal to char*

--- sdk/sim_vexv5/src/sim/physics.cpp
+++ sdk/sim_vexv5/src/sim/physics.cpp
@@ -77,30 +77,32 @@ public:
 };
 static BulletDebugDrawer_OpenGL *my_drawer;
 
-static char *line_frag_shader = "#version 330 core\n"
-                                "in vec3 fColor;\n"
-                                "out vec4 color;\n"
-                                "\n"
-                                "void main()\n"
-                                "{\n"
-                                "    color = vec4(fColor.x, fColor.y, fColor.z, 1.0);\n"
-                                "}\n"
-                                "";
-static char *line_vert_shader = "#version 330 core\n"
-                                "layout (location = 0) in vec3 position_physics;\n"
-                                "layout (location = 1) in vec3 color;\n"
-                                "out vec3 fColor;\n"
-                                "\n"
-                                "uniform mat4 projection;\n"
-                                "uniform mat4 view;\n"
-                                "\n"
-                                "void main()\n"
-                                "{\n"
-                                "   vec3 position = vec3(position_physics.x, position_physics.y, position_physics.z);"
-                                "   gl_Position = projection * view * vec4(position, 1.0f);\n"
-                                "   fColor = color;\n"
-                                "\n"
-                                "}";
+static char *line_frag_shader = (char *) "#version 330 core\n"
+                                         "in vec3 fColor;\n"
+                                         "out vec4 color;\n"
+                                         "\n"
+                                         "void main()\n"
+                                         "{\n"
+                                         "    color = vec4(fColor.x, fColor.y, fColor.z, 1.0);\n"
+                                         "}\n"
+                                         "";
+static char *line_vert_shader
+    = (char
+           *) "#version 330 core\n"
+              "layout (location = 0) in vec3 position_physics;\n"
+              "layout (location = 1) in vec3 color;\n"
+              "out vec3 fColor;\n"
+              "\n"
+              "uniform mat4 projection;\n"
+              "uniform mat4 view;\n"
+              "\n"
+              "void main()\n"
+              "{\n"
+              "   vec3 position = vec3(position_physics.x, position_physics.y, position_physics.z);"
+              "   gl_Position = projection * view * vec4(position, 1.0f);\n"
+              "   fColor = color;\n"
+              "\n"
+              "}";
 
 void sim::physics::setup()
 {
